cf55490d5f2969f74f930ffae094524eb8163a28
bum752 <bum752@gmail.com>
2021-09-15 14:29:26 +0900

    👽️ 상점 목록 조회 API url 변경

--- src/state/storeState.tsx
+++ src/state/storeState.tsx
@@ -5,7 +5,7 @@ import { store } from '../types/store';
 export const storesState = selector<store[]>({
   key: 'retrieveStoresSelector',
   get: async ({ get }) => {
-    const response = await axios.get('/mock');
+    const response = await axios.get('/stores');
 
     return response.data;
   },
