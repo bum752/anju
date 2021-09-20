f8e1978f8ce35fcf4919b22f9e585331d97799d8
bum752 <bum752@gmail.com>
2021-09-20 16:44:37 +0900

    🔥 필터 항목을 로컬스토리지에 저장해 사용하는 방식 제거

--- src/state/filterState.tsx
+++ src/state/filterState.tsx
@@ -5,12 +5,7 @@ import { filterOption } from '../types/filter';
 export const cookingMethodFilterOptionsState = selector<filterOption[]>({
   key: 'cookingMethodFilterOptionsState',
   get: async () => {
-    const cookingMethodFilterOptions = localStorage.getItem('cookingMethodFilterOptions');
-
-    if (cookingMethodFilterOptions) return JSON.parse(cookingMethodFilterOptions);
-
     const response = await axios.get('/methods');
-    localStorage.setItem('cookingMethodFilterOptions', JSON.stringify(response.data));
     return response.data;
   },
 });
@@ -18,12 +13,7 @@ export const cookingMethodFilterOptionsState = selector<filterOption[]>({
 export const sauceFilterOptionsState = selector<filterOption[]>({
   key: 'sauceFilterOptionsState',
   get: async () => {
-    const sauceFilterOptions = localStorage.getItem('sauceFilterOptions');
-
-    if (sauceFilterOptions) return JSON.parse(sauceFilterOptions);
-
     const response = await axios.get('/sauces');
-    localStorage.setItem('sauceFilterOptions', JSON.stringify(response.data));
     return response.data;
   },
 });
